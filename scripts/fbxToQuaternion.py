4ae1c704cd2c2b5c7d48273412c0f7a37d374d01
Gustavo Borba <g237965@dac.unicamp.br>
2026-05-18 00:03:07 -0300

    feat: script python para pegar quaternions das animações do fbx e colocar em arquivos .json

---
+++ scripts/fbxToQuaternion.py
@@ -0,0 +1,54 @@
+import bpy
+import os
+import json
+
+fbxFile = "/home/g237965@rsrch.cepetro.unicamp.br/concat-synth/raw_dictionary/clara_RU_salada_067_Motion.Fbx"
+fbxFileName = os.path.basename(fbxFile)
+
+bpy.ops.import_scene.fbx(filepath=fbxFile)
+armature = bpy.data.objects["Armature"]
+bones = armature.pose.bones
+
+armature.rotation_mode = "QUATERNION"
+
+scene = bpy.context.scene
+
+##### frame counting #######
+frame_start = scene.frame_start
+frame_end = scene.frame_end
+
+total_frames = frame_end - frame_start + 1
+
+##### getting quaternion data from bones in animation (order x, y, z, w) #######
+quaternion_data = {
+    "frame_count": total_frames,
+}
+
+for frame in range(total_frames):
+    scene.frame_set(frame)
+    print(f"Frame {frame}:")
+
+    frame_data = {
+        "frame_id": frame + 1
+    }
+
+    for b in bones:
+        
+        q = b.rotation_quaternion.copy()
+        print(f"    Bone {b.basename}: \n {q}")
+
+        frame_data[b.basename] = [q.x, q.y, q.z, q.w]
+
+    quaternion_data.setdefault("frame_data", []).append(frame_data)
+
+print(quaternion_data)
+for frame_data in quaternion_data["frame_data"]:
+    for key, value in frame_data.items():
+        print(f"{key}: {value}")
+
+##### putting it into json files ######
+json_filename = '/'.join([os.path.dirname(os.path.dirname(fbxFile)) ,"json_dictionary", f"{os.path.splitext(fbxFileName)[0]}.json"])
+print(json_filename)
+
+with open(json_filename, 'w') as f:
+    json.dump(quaternion_data, f, indent=4)
