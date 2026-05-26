92d0ec399f9599c1dedf1856c9f1a7996493039e
Gustavo Borba <gustavo.borba2001@gmail.com>
2026-05-26 08:58:27 -0300

    feat: transition implementation w/ bugs

--- scripts/SkeletalAnimationPlayer.cs
+++ scripts/SkeletalAnimationPlayer.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO.Pipes;
 using System.Resources;
 using System.Runtime.Serialization;
@@ -9,44 +10,53 @@ using System.Text.Json;
 public partial class SkeletalAnimationPlayer : Node3D
 {
 	[Export] public NodePath SkeletonPath;
-
-	[Export(PropertyHint.File, ".json")] public string AnimationToBePlayed = "";
 	[Export] public float AnimationSpeed = 1.0f;
+		public List<string> Animations = new List<string>
+	{
+		"res://json_dictionary/SALADA_1.json",
+		"res://json_dictionary/REPOLHO_1.json",
+		"res://json_dictionary/JUNTO-COM_1.json",
+		"res://json_dictionary/LARANJA_1.json"
+	};
 
 	private Skeleton3D skeleton;
 	private AnimationData animation;
-	private string NextAnimationToBePlayed = "";
-	private string PreviousAnimation = "";
+	private List<FrameInfo> transitionFrames = new();
+	private bool playingTransition = false;
+	private int transitionFrame = 0;
 	private int currentFrame = 0;
 	private int frameCount = 0;
 	const float FRAME_TIME = 1f / 30f;
 	private float timer = 0;
 	private bool startPressed = false;
-	
+	private int currentAnimationIndex = 0;
 
 	public override void _Ready()
 	{
-		Button startButton = GetNode<Button>("CanvasLayer/Button");
-		startButton.Pressed += OnButtonPressed;
+		GD.Print("INICIALIZANDO.......");
+		// Button startButton = GetNode<Button>("CanvasLayer/Button");
+		GD.Print("fafwkalfklw");
+
+		// startButton.Pressed += OnButtonPressed;
 
 		skeleton = GetNode<Skeleton3D>(SkeletonPath);
 
-		if (skeleton == null)
-		{
-			GD.PrintErr("Skeleton NOT FOUND");
-			return;
-		}
-
 		GD.Print($"Skeleton loaded: {skeleton.Name}");
-
-		PreviousAnimation = AnimationToBePlayed;
-		LoadAnimation();
+		
+		LoadGloss();
 	}
 
+	/// <summary>
+	/// Falta implementar:
+	/// 	- UI com seleção da ordem de glosas para testes
+	/// 	- Animação de repouso toda vez que tiver Idle
+	/// 	
+	/// </summary>
+	/// <param name="delta"></param>
 	public override void _Process(double delta)
 	{
-		if (startPressed)
-		{
+		// if (startPressed)
+		// {
 			if (animation == null)
 			{
 				GD.Print("Animation is NULL");
@@ -59,31 +69,34 @@ public partial class SkeletalAnimationPlayer : Node3D
 			// aplica os dados de quaternion a cada osso naquele frame especificado, controlado pela variável timer
 			while (timer >= FRAME_TIME)
 			{
-				GD.Print($"Playing frame {currentFrame}");
-
-				ApplyFrame(currentFrame);
-
-				currentFrame++;
-
-				if (currentFrame >= animation.FrameData.Count)
+				if (playingTransition)
 				{
-					NextAnimationToBePlayed = "res://json_dictionary/clara_RU_sobremesa_025_Motion.json";
+					ApplyTransition();
+				} else {
+			
+					GD.Print($"Playing frame {currentFrame}");
 					
-					// MUDAR AQUI DEPOIS PRA FICAR INDO DE UMA PRA OUTRA ANIMAÇÃO
-					if (NextAnimationToBePlayed != AnimationToBePlayed) {
-						GD.Print("Next Animation");
-						PreviousAnimation = AnimationToBePlayed;
-						AnimationToBePlayed = NextAnimationToBePlayed;
-						LoadAnimation();
-					} else {
-						GD.Print("Looping animation");
-					}
-					currentFrame = 0;
-				}
-				timer -= FRAME_TIME;
+					ApplyFrame(currentFrame);
+					currentFrame++;
+					
+					if (currentFrame >= animation.FrameData.Count)
+					{
+						
+						FrameInfo prevlast = animation.FrameData[^1];
+						
+						LoadGloss();
 
+						FrameInfo nextFirst = animation.FrameData[0];
+
+						LoadTransition(prevlast, nextFirst);
+
+						currentFrame = 0;
+					}
+				}
+
+				timer -= FRAME_TIME;
 			}
-		}
+		// }
 	}
 
 	private void OnButtonPressed() 
@@ -92,37 +105,25 @@ public partial class SkeletalAnimationPlayer : Node3D
 		GD.Print("Started!");
 	}
 
-	void LoadAnimation()
+	void LoadGloss()
 	{
+
+		string animationPath = Animations[currentAnimationIndex];
+
 		GD.Print("=== LOADING JSON ===");
 
-		if (!FileAccess.FileExists(AnimationToBePlayed))
+		if (!FileAccess.FileExists(animationPath))
 		{
-			GD.PrintErr($"File not found: {AnimationToBePlayed}");
+			GD.PrintErr($"File not found: {animationPath}");
 			return;
 		}
 
-		string json =
-			FileAccess.GetFileAsString(
-				AnimationToBePlayed
-			);
-
-		GD.Print($"JSON length: {json.Length}");
 
 		try
 		{
-			animation =
-				JsonSerializer.Deserialize<AnimationData>(
-					json
-				);
+			string json = FileAccess.GetFileAsString(animationPath);
 
-			if (animation == null)
-			{
-				GD.PrintErr("Deserialize returned NULL");
-				return;
-			}
-
-			GD.Print($"FrameCount={animation.FrameCount}");
+			animation = JsonSerializer.Deserialize<AnimationData>(json);
 
 			if (animation.FrameData == null)
 			{
@@ -130,9 +131,12 @@ public partial class SkeletalAnimationPlayer : Node3D
 				return;
 			}
 
-			GD.Print(
-				$"Loaded {animation.FrameData.Count} frames"
-			);
+			currentAnimationIndex++;
+
+			if (currentAnimationIndex >= Animations.Count)
+			{
+				currentAnimationIndex = 0;
+			}
 		}
 		catch (Exception e)
 		{
@@ -144,47 +148,15 @@ public partial class SkeletalAnimationPlayer : Node3D
 	{
 		GD.Print($"Applying frame {FrameId}");
 
-		if (FrameId >= animation.FrameData.Count)
-		{
-			GD.PrintErr("Frame out of range");
-			return;
-		}
-
 		FrameInfo frame = animation.FrameData[FrameId];
-
-		// GD.Print($"Bone List: {string.Join(", ", frame.Bones.Keys)}");
-
-		// GD.Print($"Bone Count: {frame.Bones.Count}");
-
-		if (frame.Bones == null)
-		{
-			GD.PrintErr("Frame has NULL bones");
-			return;
-		}
-
-		int applied = 0;
 
 		foreach (var bone in frame.Bones)
 		{
-			string boneName =
-				bone.Key;
+			string boneName = bone.Key;
 
 			int id = skeleton.FindBone(boneName);
 
-			if (id == -1)
-			{
-				GD.Print($"Bone not found: {boneName}");
-
-				continue;
-			}
-
 			float[] q = bone.Value;
-
-			if (q.Length != 4)
-			{
-				GD.PrintErr($"Invalid quaternion {boneName}");
-				continue;
-			}
 
 			// quaternion -> x, y , z, w
 			Quaternion quat =
@@ -200,12 +172,112 @@ public partial class SkeletalAnimationPlayer : Node3D
 		// aplica o quaternion em relação à posição de referência (rest) do modelo
     skeleton.SetBonePoseRotation(id, rest * quat);
 
-			applied++;
+		}
+	}
+		void LoadTransition(FrameInfo prevLastFrame, FrameInfo nextFirstFrame)
+	{
+	/*
+    1. Iterar a lista de animações escolhidas
+		2. Pegar animationToBePlayed = lista[0]
+		3. Fazer LoadGloss()
+		4. Pegar o float[] animation.FrameData[animation.FrameCount - 1].Bones.Values
+		5. Pegar 
+  */
+		transitionFrames.Clear();
+
+		int numOfInterpolationFrames = 10;
+		
+		for (int i = 1; i <= numOfInterpolationFrames; i++)
+		{
+			float weight = i / (float) (numOfInterpolationFrames + 1);
+
+			FrameInfo transitionFrame = new FrameInfo
+			{
+				FrameId = i,
+				AdditionalData = new Dictionary<string, JsonElement>()
+			};
+		
+			Dictionary<string, float[]> bones = new();
+
+			foreach (var bone in prevLastFrame.Bones)
+			{
+				string boneName = bone.Key;
+
+				float[] prev = bone.Value;
+				float[] next = nextFirstFrame.Bones[boneName];
+
+				Quaternion q1 = new Quaternion(
+					prev[0],
+					prev[1],
+					prev[2],
+					prev[3]
+				);
+
+				Quaternion q2 = new Quaternion(
+					next[0],
+					next[1],
+					next[2],
+					next[3]
+				);
+
+				Quaternion resultingQuat = q1.Slerp(q2, weight);
+
+				bones[boneName] = [resultingQuat.X, resultingQuat.Y, resultingQuat.Z, resultingQuat.W];
+			}
+
+			transitionFrame.AdditionalData = ConvertToJsonElements(bones);
+			transitionFrames.Add(transitionFrame);
 		}
 
-		GD.Print(
-			$"Applied {applied} bones"
-		);
+		transitionFrame = 0;
+		playingTransition = true;
 	}
 
+	void ApplyTransition()
+{
+    if (transitionFrame >= transitionFrames.Count)
+    {
+        playingTransition = false;
+        return;
+    }
+
+    FrameInfo frame = transitionFrames[transitionFrame];
+
+    foreach (var bone in frame.Bones)
+    {
+        string boneName = bone.Key;
+
+        int id = skeleton.FindBone(boneName);
+
+        if (id == -1)
+				{
+					continue;
+				}
+
+        float[] q = bone.Value;
+
+        Quaternion quat = new Quaternion(
+                q[0],
+                q[1],
+                q[2],
+                q[3]
+            );
+
+        Quaternion rest = skeleton.GetBoneRest(id).Basis.GetRotationQuaternion();
+
+        skeleton.SetBonePoseRotation(id, rest * quat);
+    }
+    transitionFrame++;
+}
+	Dictionary<string, JsonElement> ConvertToJsonElements(Dictionary<string, float[]> bones)
+	{
+		var dict = new Dictionary<string, JsonElement>();
+
+		foreach (var kv in bones)
+		{
+				dict[kv.Key] = JsonSerializer.SerializeToElement(kv.Value);
+		}
+
+		return dict;
+	}
 }
--- scripts/TransitionAnimation.cs
+++ scripts/TransitionAnimation.cs
@@ -1,4 +1,14 @@
+using System.Collections.Generic;
+using Godot;
+
 public class TransitionAnimation
 {
+  /*
+- Calcular todas as transições entre glosas
+						- Fazer LoadGloss() pra cada dupla adjacente
+							- Inicialmente pegar o último dicionário (nome: quat) do primeiro, dar LoadGloss() pro segundo e pegar o primeiro frame do segundo
+							- Após isso, converte os dois para Quaternions, e armazena o slerp entre os dois em uma List<> ou array
+  */
+
   
 }
